cdfcad1505b12bdb3a5a634f1e55c53e10460248
Dylan Vann <dylan.vann@gmail.com>
2018-03-18 05:34:48 -0400

    Upgrade to glide 4 with progress listeners working.

---
+++ android/src/main/java/com/dylanvann/fastimage/MyAppGlideModule.java
@@ -0,0 +1,8 @@
+package com.dylanvann.fastimage;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public final class MyAppGlideModule extends AppGlideModule {
+}
